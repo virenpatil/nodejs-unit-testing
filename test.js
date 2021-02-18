c962e0d7e26a81d6f7e1527a5ecedcde38350a1a
oe-virendra <virendra,patil@objectedge.com>
2021-02-18 17:56:03 +0530

    setup of mocha

---
+++ test.js
@@ -0,0 +1,25 @@
+
+/* 
+    Basic of the mocha.js
+        install the mocha using following command:
+            npm install --global mocha
+        as a development dependency for your project:
+         npm install --save-dev mocha
+ */
+
+
+const assert = require('assert');
+
+describe('file to be tested',() =>{
+    context('function to be tested', ()=>{
+        it('should do something',()=>{
+            // assert.equal(1,2);
+            assert.equal(1,1);
+        })
+        it('should do something else ', ()=>{
+            // assert.deepEqual({name: 'joe'},{name:'steve'});
+            assert.deepEqual({name: 'joe'},{name:'joe'});
+        })
+        it('this is pendding test',)
+    })
+})
